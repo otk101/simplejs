58442960bdcf36945dfb864b132c8dc71eb6d79b
K Ort <tegakenor@gmail.com>
2023-01-01 15:28:49 -0500

    adding simple javascript code assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function displayInfo(name, height, country) {
+  console.log(`Name: ${name}`);
+  console.log(`Height: ${height}`);
+  console.log(`Country: ${country}`);
+}
+
+displayInfo('Kay', '5.5ft', 'America');
+
